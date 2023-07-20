bff667cdb40467b4f5f0a483dbcc031741ef0a37
Lava4creeper <80729894+Lava4creeper@users.noreply.github.com>
2023-07-20 21:30:49 +0000

    Added a drop-down optionmenu for selecting level.

--- main.py
+++ main.py
@@ -4,14 +4,71 @@ from tkinter import *
 class main_menu:
 
   def __init__(self):
-    print("hello world")
-    button_font = ("Arial", "12", "bold")
+    
+    level_selected = ""
+    description = "This is a brief description of how this program works."
+    
+    button_font = ("Arial", "8", "bold")
     button_fg = "#FFFFFF"
+    
     self.main_frame = Frame(padx=10, pady=10)
     self.main_frame.grid()
 
     self.main_heading = Label(self.main_frame, text="Hello World", font=("Arial", "16", "bold"))
     self.main_heading.grid(row=0)
+
+    
+    self.main_description = Label(self.main_frame, text=description, wrap=250, width=40, justify="left")
+    self.main_description.grid(row=1)
+
+    self.button_frame = Frame(self.main_frame)
+    self.button_frame.grid(row=2)
+
+    self.instructions_button = Button(self.button_frame, 
+                                     text="Instructions",
+                                     bg="#990099",
+                                     fg=button_fg,
+                                     font=button_font,
+                                     width=12)
+    self.instructions_button.grid(row=0, column=0)
+
+    self.quiz_contents_button = Button(self.button_frame,
+                                      text="Quiz Contents",
+                                      bg="#990099",
+                                      fg=button_fg,
+                                      font=button_font,
+                                      width=12)
+    self.quiz_contents_button.grid(row=0, column=1)
+
+    self.settings_button = Button(self.button_frame,
+                                 text="Instructions",
+                                 bg="#990099",
+                                 fg=button_fg,
+                                 font=button_font,
+                                 width=12)
+    self.settings_button.grid(row=0, column=2)
+
+    level_options = [
+      "Year 1",
+      "Year 2",
+      "Year 3",
+      "Year 4",
+      "Year 5",
+      "Year 6",
+      "Year 7",
+      "Year 8",
+      "Year 9",
+      "Year 10",
+      "Year 11",
+      "Year 12",
+      "Year 13",
+      "Custom"
+    ]
+    
+    default = StringVar()
+    default.set( "Input Quiz Level" )
+    self.level_input = OptionMenu(self.main_frame, default, *level_options)
+    self.level_input.grid(row=3)
 if __name__ == "__main__":
   window = Tk()
   window.title("Main Menu")
