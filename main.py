0dddb47afdb5c7bb2846b347cd2d58a6981cb845
Lava4creeper <80729894+Lava4creeper@users.noreply.github.com>
2023-07-26 03:09:50 +0000

    added functionality to all buttons; console now prints what's been pressed and the selected level. This will help sense where to send the user based on input.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+# Import statements
 from tkinter import *
 
+# Define main menu class
 class main_menu:
-
+  #Upon initialisation
   def __init__(self):
-    
-    level_selected = ""
+
+    #Initialise variables
     description = "This is a brief description of how this program works."
     button_font = ("Arial", "8", "bold")
     button_fg = "#000000"
@@ -28,105 +30,144 @@ class main_menu:
     ]
 
     selected_level = StringVar()
-    selected_level.set( "Input Quiz Level" )
+    selected_level.set("Input Quiz Level")
+    print(selected_level.get())
     
+    # Form a frame to place all elements on
     self.main_frame = Frame(padx=10, pady=10)
     self.main_frame.grid()
 
+    # Create and place the heading of the page
     self.main_heading = Label(self.main_frame, text="Hello World", font=("Arial", "16", "bold"))
     self.main_heading.grid(row=0)
 
-    
+    # Create and place a brief description of the quiz
     self.main_description = Label(self.main_frame, text=description, wrap=250, width=40, justify="left")
     self.main_description.grid(row=1)
 
-    self.button_frame = Frame(self.main_frame)
-    self.button_frame.grid(row=2)
+    # Create and place a frame to place the Instructions, Quiz Contents and Settings buttons within
+    self.instructions_contents_settings_frame = Frame(self.main_frame)
+    self.instructions_contents_settings_frame.grid(row=2)
 
-    self.quiz_type_frame = Frame(self.main_frame)
-    self.quiz_type_frame.grid(row=4)
-
-    self.history_close_frame = Frame(self.main_frame)
-    self.history_close_frame.grid(row=7)
-
-    self.instructions_button = Button(self.button_frame, 
+    # Create and place an instructions button within the previously created frame
+    self.instructions_button = Button(self.instructions_contents_settings_frame, 
                                      text="Instructions",
                                      bg="#A1E887",
                                      fg=button_fg,
                                      font=button_font,
-                                     width=12)
+                                     width=12,
+                                     command=lambda: self.button_pressed("instructions", selected_level.get()))
     self.instructions_button.grid(row=0, column=0)
 
-    self.quiz_contents_button = Button(self.button_frame,
+    # Create and place a Quiz Contents button within the previously created frame
+    self.quiz_contents_button = Button(self.instructions_contents_settings_frame,
                                       text="Quiz Contents",
                                       bg="#A1E887",
                                       fg=button_fg,
                                       font=button_font,
-                                      width=12)
+                                      width=12,
+                                      command=lambda: self.button_pressed("contents", selected_level.get()))
     self.quiz_contents_button.grid(row=0, column=1)
 
-    self.settings_button = Button(self.button_frame,
+    # Create and place a settings button within the previously created frame
+    self.settings_button = Button(self.instructions_contents_settings_frame,
                                  text="Settings",
                                  bg="#A1E887",
                                  fg=button_fg,
                                  font=button_font,
-                                 width=12)
+                                 width=12,
+                                 command=lambda: self.button_pressed("settings", selected_level.get()))
     self.settings_button.grid(row=0, column=2)
-    
-    self.level_input = OptionMenu(self.main_frame, selected_level, *level_options,)
+
+    #Create and place a drop-down menu with the level options for the quiz
+    self.level_input = OptionMenu(self.main_frame, selected_level, *level_options)
     self.level_input.config(bg="#A1E887", width=42, fg="#000000", font=("arial", "8", "bold"))
     self.level_input.grid(row=3)
 
+    # Create and place a frame to place the quiz type buttons within
+    self.quiz_type_frame = Frame(self.main_frame)
+    self.quiz_type_frame.grid(row=4)
+
+    # Create and place within the previously created frame a multiple choice button
     self.multiple_choice_button = Button(self.quiz_type_frame,
                                         text="Multiple Choice",
                                         bg="#A1E887",
                                         fg=button_fg,
                                         font=button_font,
-                                        width=20)
+                                        width=20,
+                                        command=lambda: self.button_pressed("multiple choice", selected_level.get()))
     self.multiple_choice_button.grid(column=0, row=0)
 
+    # Create and place within the previously created frame a typed input button
     self.typed_input_button = Button(self.quiz_type_frame,
                                     text="Typed Input",
                                     bg="#A1E887",
                                     fg=button_fg,
                                     font=button_font,
                                     width=20,
-                                    state=DISABLED)
+                                    state=DISABLED,
+                                    command=lambda: self.button_pressed("typed input", selected_level.get()))
     self.typed_input_button.grid(column=1, row=0)
 
+    # Create and place a Begin Quiz button]
     self.begin_quiz_button = Button(self.main_frame,
                                    text="Begin Quiz",
                                    bg="#80B192",
                                    fg=button_fg,
                                    font=("Arial", "30", "bold"),
-                                   width=11)
+                                   width=11,
+                                   command=lambda: self.button_pressed("start", selected_level.get()))
     self.begin_quiz_button.grid(row=5)
 
+    # Create and display a label with the highest and average scores
     self.scores_display = Label(self.main_frame,
                                text="High Score: {}        Average Score: {}".format(high_score, mean_score),
                                fg="#9C0000")
     self.scores_display.grid(row=6)
 
+    # Create and place a frame for the history and close buttons
+    self.history_close_frame = Frame(self.main_frame)
+    self.history_close_frame.grid(row=7)
+
+    # Create and place a history/export button within the previously created frame
     self.history_button = Button(self.history_close_frame,
                                 text="History/Export",
                                 bg="#A1E887",
                                 fg=button_fg,
                                 font=button_font,
-                                width=20)
+                                width=20,
+                                command=lambda: self.button_pressed("history", selected_level.get()))
     self.history_button.grid(row=0, column=0)
 
+    # Create and place a Close button within the previously created frame
     self.close_button = Button(self.history_close_frame,
                                 text="Close",
                                 bg="#A1E887",
                                 fg=button_fg,
                                 font=button_font,
-                                width=20)
+                                width=20,
+                              command=lambda: self.button_pressed("close", selected_level.get()))
     self.close_button.grid(row=0, column=1)
-    
+
+  def button_pressed(self, button, level):
+    print(button)
+    if button == "multiple choice":
+      self.multiple_choice_button.config(state=DISABLED)
+      self.typed_input_button.config(state=NORMAL)
+    elif button == "typed input":
+      self.typed_input_button.config(state=DISABLED)
+      self.multiple_choice_button.config(state=NORMAL)
+    print(level)
+
+#**************Main Routine******************
+
+# Check that code has been run directly by the interpreter
 if __name__ == "__main__":
+  #Create, name and size the window
   window = Tk()
   window.title("Main Menu")
   window.geometry("430x900")
-  # indow.configure(bg="#6A8D92")
+  #window.configure(bg="#6A8D92")
+  # open main menu
   main_menu()
   window.mainloop()
